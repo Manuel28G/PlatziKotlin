d693913eb7cef7aa74b2eee99fcff24263c1791b
Manuel Goncalves <man281192@gmail.com>
2020-03-01 00:47:30 -0300

    Using strings

--- src/HolaMundo.kt
+++ src/HolaMundo.kt
@@ -5,7 +5,7 @@ fun main() {
     println(4*2)
     println(4/2)
     println(4%2)
-    val a = 4
+    var a = 4
     val b = 2
     println("*********Using object functions**********")
     println(a.plus(b))
@@ -13,5 +13,24 @@ fun main() {
     println(a.times(b))
     println(a.div(b))
     println(a.rem(b))
+    a = 5
+    println("Valor antes de incrementar a: $a")
+    println(a.inc())
+    println("Valor despues de incrementar a: $a")
+    println(a.dec())
+    println("Valor despues de decrementar a: $a")
 
+    //RAW String
+    val parrafo ="""
+        ** Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        ** magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        ** consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        ** Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    """
+    println("C**********Con indentado ******************")
+    println(parrafo)
+    println("C**********Sin indentado ******************")
+    println(parrafo.trimIndent())
+    println("************Removing trash elemnts***********")
+    println(parrafo.trimMargin("** "))
 }
